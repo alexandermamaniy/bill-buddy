2de147c5d40e287d1e6639ee833bf90735dd3eac
Alexander Mamani Yucra <akeymy4@gmail.com>
2024-07-23 17:37:52 +0100

    set app

---
+++ billbuddy/users/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+        user = self.model(
+            email = email,
+            is_staff = is_staff,
+            is_superuser = is_superuser,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
+
+    def create_superuser(self, email,  password=None, **extra_fields):
+        return self._create_user(email, password, True, True, **extra_fields)
+
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.CharField(max_length = 255, unique = True)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def natural_key(self):
+        return (self.email)
+
+    def __str__(self):
+        return f'{self.email}'
