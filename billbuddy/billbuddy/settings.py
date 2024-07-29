363d326eed00962ef01a06a9527b2178944a6fe3
Alexander Mamani Yucra <akeymy4@gmail.com>
2024-07-29 11:25:31 +0100

    Feat: Created Profile model to register new users on the system

--- billbuddy/billbuddy/settings.py
+++ billbuddy/billbuddy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #LOCAL_APPS
     'users',
+    'buddy_profiles',
     #THRID_APPS
     'rest_framework',
     'corsheaders',
@@ -173,3 +174,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # use custom authentications
 AUTH_USER_MODEL = 'users.User'
+
+# Actual directory user files go to
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+
+# URL used to access the media
+MEDIA_URL = '/media/'
--- billbuddy/billbuddy/urls.py
+++ billbuddy/billbuddy/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -30,4 +34,4 @@ urlpatterns = [
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name="schema")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
