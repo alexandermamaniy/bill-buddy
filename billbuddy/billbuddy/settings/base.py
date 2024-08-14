94f9e7887f651682b4e5f40ffc7ff372c86ae794
Alexander Mamani <akeymy4@gmail.com>
2024-08-14 12:29:38 +0100

    installed nginx

--- billbuddy/billbuddy/settings/base.py
+++ billbuddy/billbuddy/settings/base.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
