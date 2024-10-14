c55710c682c1a0eafe1ad3d3185797e27efb34ec
Alexander Mamani Yucra <akeymy4@gmail.com>
2024-10-14 15:52:58 +0100

    feature: add Update, Delete group

--- billbuddy/buddy_groups/serializers.py
+++ billbuddy/buddy_groups/serializers.py
@@ -63,15 +63,21 @@ class BuddyGroupSerializer(ModelSerializer):
         group_admins_data = self.context.get('group_admins')
 
         if group_members_data is not None:
+            logger.info("ingres  + "+ str(group_admins_data))
             instance.groupmembers_set.all().delete()
             for member_data in group_members_data:
                 buddy_profile_member = BuddyProfile.objects.get(id=member_data)
-                GroupMembers.objects.create(group_belong_to=instance, buddy_profile_member=buddy_profile_member)
+                group_member_d = GroupMembers.objects.create(group_belong_to=instance,
+                                                             buddy_profile_member=buddy_profile_member)
+                group_member_d.save()
 
         if group_admins_data is not None:
             instance.groupadmins_set.all().delete()
             for admin_data in group_admins_data:
                 buddy_profile_admin = BuddyProfile.objects.get(id=admin_data)
-                GroupAdmins.objects.create(group_belong_to=instance, buddy_profile_admin=buddy_profile_admin)
+                group_admin_d = GroupAdmins.objects.create(group_belong_to=instance,
+                                                           buddy_profile_admin=buddy_profile_admin)
+                group_admin_d.save()
 
+        instance.save()
         return instance
